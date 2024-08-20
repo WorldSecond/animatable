6835fd666c9347f457f50790bbbda138d9ac8b48
zhang lu <startalker5267@163.com>
2024-08-20 11:48:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function $$(expr, con) { return [].slice.call((con || document).querySelectorAll
 
 var css = [];
 
-$$('a[data-property]').forEach(function(el, i){
+`$$('a[data-property]').forEach(function(el, i){
 	var property = el.getAttribute('data-property'),
 		from = el.getAttribute('data-from'),
 		to = el.getAttribute('data-to');
